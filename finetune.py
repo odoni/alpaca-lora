b22ac21ead45c63e522b2db0395cec3877212e1f
felipe.odoni <felipe.odoni@gmail.com>
2023-03-28 00:30:05 +0100

    data factor complete

--- finetune.py
+++ finetune.py
@@ -1,5 +1,4 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import sys
 from typing import List
@@ -82,8 +81,7 @@ def train(
     model = LlamaForCausalLM.from_pretrained(
         base_model,
         load_in_8bit=True,
-        max_memory={0: "10GiB", 1: "6GiB", "cpu": "20GiB"},
-        device_map={'':0}
+        device_map=device_map
     )
 
     tokenizer = LlamaTokenizer.from_pretrained(base_model)
